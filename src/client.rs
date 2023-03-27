42f92d7d4454b6feafff3e1aa6cd219a258b3d9c
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-27 08:36:37 -0300

    Update client.rs

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,17 @@
-use rgb::
-use carbonado::
-use storm::
+use carbonado::Repository;
+use storm::Client;
+use rgb::Network;
+
+fn main() {
+    // Connect to a Carbonado repository
+    let repo = Repository::connect("postgres://localhost/mydb").unwrap();
+
+    // Connect to a Storm server
+    let client = Client::connect("127.0.0.1:6700").unwrap();
+
+    // Connect to the Lightning Network via RGB
+    let network = Network::connect("https://my-lnd-node.com").unwrap();
+
+    // Use the protocols
+    // ...
+}
